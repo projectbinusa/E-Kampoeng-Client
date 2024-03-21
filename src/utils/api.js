e08d8625bbe7f12db98abfb79fb37cbbdb1bd3ee
melyanahasya <melysasa49@gmail.com>
2024-03-21 13:56:34 +0700

    page e-kas

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,8 @@ export const api_register = `${base_api}/register`;
 export const api_warga = `${base_api_ekampoeng}/warga`;
 export const api_organisasi = `${base_api_ekampoeng}/organisasi`;
 export const api_soerat = `${base_api_ekampoeng}/e-soerat/`;
-export const api_tag= `${base_api_ekampoeng}/tags-berita/`;
-export const api_category= `${base_api_ekampoeng}/category-berita/`;
+export const api_tag = `${base_api_ekampoeng}/tags-berita/`;
+export const api_category = `${base_api_ekampoeng}/category-berita/`;
+export const api_kas = `${base_api_ekampoeng}/e-kas/`;
 
 
